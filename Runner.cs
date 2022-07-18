b9c33daeaf272660a5cf6e3f691ed5b0d9a2c7d4
Greycell <75478778+Greycell-lab@users.noreply.github.com>
2022-07-18 14:42:35 +0200

    Add files via upload

---
+++ Runner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Run
+{
+    class Runner
+    {
+        public int x=0;
+        public int y=0;
+               
+        public Runner(int X, int Y)
+        {
+            x = X;
+            y = Y;
+        }
+        public int RunX()
+        {
+            x++;
+            return x;
+        }
+        public int RunY()
+        {
+            y++;
+            return y;
+        }
+        public int BackX()
+        {
+            x--;
+            return x;
+        }
+        public int BackY()
+        {
+            y--;
+            return y;
+        }
+        public void RunR()
+        {
+            x = 0;
+            y = 0;
+        }
+    }
+}
