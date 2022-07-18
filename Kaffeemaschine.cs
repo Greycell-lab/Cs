b6d7f4c1774a810b1b9d6886c5f6e91f39743b4c
Greycell <75478778+Greycell-lab@users.noreply.github.com>
2022-07-18 19:41:51 +0200

    Add files via upload

---
+++ Kaffeemaschine.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffeemaschine
+{
+    class Kaffeemaschine
+    {       
+        double _wasser = 0;
+        double _bohnen = 0;
+        double _kaffeemenge = 0;
+        public double Wasser;
+        public double Bohnen;
+        public double Kaffeemenge;
+        static double _maxWasser = 2.5;
+        static double _maxBohnen = 2.5;
+        public Kaffeemaschine()
+        {           
+            Wasser = _wasser;
+            Bohnen = _bohnen;
+            Kaffeemenge = _kaffeemenge;
+        }
+        public double wasserAuffüllen(double menge)
+        {
+            if(menge > _maxWasser)
+            {
+                Console.WriteLine("Zu viel!");
+            }
+            else
+            {
+                _wasser += menge;
+            }
+            return _wasser;
+        }
+        public double bohnenAuffüllen(double menge)
+        {
+            if (menge > _maxBohnen)
+            {
+                Console.WriteLine("Zu viel!");
+            }
+            else
+            {
+                _bohnen += menge;                
+            }
+            return _bohnen;
+        }
+        public bool macheKaffee(double menge, double verhaeltnisWasserBohnen)
+        {
+            if(verhaeltnisWasserBohnen == 1)
+            {                
+                if(_wasser-menge>0 && _bohnen-menge>0)
+                {
+                    _wasser -= menge;
+                    _bohnen -= menge;
+                    _kaffeemenge++;
+                    return true;
+                }
+            }
+            if (verhaeltnisWasserBohnen == 2)
+            {
+                if (_wasser - menge > 0 && _bohnen - menge/2 > 0)
+                {
+                    _wasser -= menge;
+                    _bohnen -= menge/2;
+                    _kaffeemenge++;
+                    return true;
+                }
+            }
+            if (verhaeltnisWasserBohnen == 0.5)
+            {
+                if (_wasser - menge > 0 && _bohnen - menge*2 > 0)
+                {
+                    _wasser -= menge;
+                    _bohnen -= menge*2;
+                    _kaffeemenge++;                   
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Kaffeemaschine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kaffeemaschine Bosch = new Kaffeemaschine();                   
+            Bosch.macheKaffee(0.25, 1);
+            Console.WriteLine($"Wasser: {Bosch.Wasser}\nBohnen: {Bosch.Bohnen}\nKaffeemenge: {Bosch.Kaffeemenge}");
+        }
+    }
+}
